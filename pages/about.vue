ace6ddba82d0e6c9b6afedd79b78a5a4c69e9932
Francisco Bejarano <tronquinwantsmoney@gmail.com>
2019-05-08 20:59:06 -0400

    Initial Commit

---
+++ pages/about.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>
+      Single Bees are looking for Big juicy fat
+      <span>Flowers!</span>
+    </h1>
+    <p>Bananas are cute too</p>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+h1 {
+  color: pink;
+  font-weight: normal;
+}
+
+p {
+  color: gold;
+}
+
+span {
+  font-weight: bold;
+}
+</style>
