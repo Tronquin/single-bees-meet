a082a966bf1d963ef376c6715bfe0148d6ae9736
Francisco Bejarano <tronquinwantsmoney@gmail.com>
2019-06-23 01:05:50 -0400

    page name changed

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,7 @@ module.exports = {
    ** Headers of the page
    */
   head: {
-    title: pkg.name,
+    title: 'Single Bees Meet Buzz',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
